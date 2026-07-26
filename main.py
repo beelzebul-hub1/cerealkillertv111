de8b8ed75fbfbab5474cc8397aa2e9a52c606757
beelzebul-hub1 <stephendavis664+1@gmail.com>
2026-07-26 10:13:38 -0500

    Update Twitch miner credentials and API endpoint

--- main.py
+++ main.py
@@ -42,8 +42,8 @@ threading.Thread(target=run_flask, daemon=True).start()
 
 # ---------------- MINER ----------------
 twitch_miner = TwitchChannelPointsMiner(
-    username="cerealkillertv111",
-    password="x4%2dQtXEx#CWgjHyQAP",
+    username="Kryphic348156",
+    password="Work7-Enlisted1-Overboard9-Threaten8-Cope8",
     claim_drops_startup=False,
     priority=[
         Priority.STREAK,
@@ -134,7 +134,7 @@ def export_points_loop():
             # send to website
             try:
                 requests.post(
-                    "https://render-b1yk.onrender.com/api/update",
+                    "https://render-1-ethy.onrender.com/api/update",
                     json=data,
                     timeout=10
                 )
@@ -156,7 +156,7 @@ threading.Thread(target=export_points_loop, daemon=True).start()
 # ---------------- RUN -----------------
 twitch_miner.mine(
     [
-        Streamer("imshayph"),
+        Streamer("shayph"),
         Streamer("sirdapper"),
         Streamer("waffletrades_"),
     ],
